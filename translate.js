b740cd1e6868c1420c6dc1534bd0f8de13e5cd13
Tseten <tseten.lama@uipath.com>
2021-02-19 16:58:08 -0500

    Translate to multiple langs for given json

--- translate.js
+++ translate.js
@@ -9,84 +9,100 @@ const subscriptionKey = process.env.SUBSCRIPTION_KEY;
 const endpoint = process.env.ENDPOINT;
 const region = process.env.REGION;
 
-let headerJSONOg = require("./header.json");
-let headerJSON = _.cloneDeep(headerJSONOg);
+const fileName = process.argv[2];
 
-let allTexts = [];
-let oldToNewVal = {};
+let JSONOg = require(`./${fileName}`);
 
-const lang = process.argv[2];
+function translate(lang) {
+  let JSON = _.cloneDeep(JSONOg);
 
-function createTexts(json) {
-  Object.keys(json).forEach((key) => {
-    if (key === "labelColor" || key === "lang" || key === "href") {
-      delete json[key];
-    }
-  });
-  Object.values(json).forEach((value) => {
-    if (typeof value === "string") {
-      allTexts.push({ text: value });
-    } else if (typeof value === "object") {
-      createTexts(value);
-    } else if (Array.isArray(value)) {
-      value.forEach((val) => {
-        createTexts(val);
-      });
-    }
-  });
-}
+  let allTexts = [];
+  let oldToNewVal = {};
 
-function translateText(texts, i, lang) {
-  let options = {
-    method: "POST",
-    baseUrl: endpoint,
-    url: "translate",
-    qs: {
-      "api-version": "3.0",
-      to: [lang],
-    },
-    headers: {
-      "Ocp-Apim-Subscription-Key": subscriptionKey,
-      "Ocp-Apim-Subscription-Region": region,
-      "Content-type": "application/json",
-      "X-ClientTraceId": uuidv4().toString(),
-    },
-    body: texts,
-    json: true,
-  };
-
-  request(options, function (err, res, body) {
-    body.forEach((result, j) => {
-      oldToNewVal[allTexts[i + j].text] = result.translations[0].text;
+  function createTexts(json) {
+    Object.keys(json).forEach((key) => {
+      if (
+        key === "labelColor" ||
+        key === "lang" ||
+        key === "href" ||
+        key === "target"
+      ) {
+        delete json[key];
+      }
     });
-  }).then((res2) => {
-    updateOriginalJSON(headerJSONOg);
-    let data = JSON.stringify(headerJSONOg);
-    fs.writeFileSync(`header.${lang}.json`, data);
-  });
-}
-
-createTexts(headerJSON);
-
-for (let i = 0; i < allTexts.length; i += 100) {
-  translateText(allTexts.slice(i, i + 100), i, lang);
-}
-
-function updateOriginalJSON(json) {
-  Object.keys(json).forEach((key) => {
-    if (key !== "labelColor" && key !== "lang" && key !== "href") {
-      let ogVal = json[key];
-      if (typeof ogVal === "string") {
-        if (oldToNewVal[ogVal]) {
-          json[key] = oldToNewVal[ogVal];
-        }
-      } else if (typeof ogVal === "object") {
-        updateOriginalJSON(ogVal);
-      } else if (Array.isArray(ogVal)) {
-        ogVal.forEach((val) => {
-          updateOriginalJSON(val);
+    Object.values(json).forEach((value) => {
+      if (typeof value === "string") {
+        allTexts.push({ text: value });
+      } else if (typeof value === "object") {
+        createTexts(value);
+      } else if (Array.isArray(value)) {
+        value.forEach((val) => {
+          createTexts(val);
         });
       }
-    }
-  });
+    });
+  }
+
+  function translateText(texts, i, lang) {
+    let options = {
+      method: "POST",
+      baseUrl: endpoint,
+      url: "translate",
+      qs: {
+        "api-version": "3.0",
+        to: [lang],
+      },
+      headers: {
+        "Ocp-Apim-Subscription-Key": subscriptionKey,
+        "Ocp-Apim-Subscription-Region": region,
+        "Content-type": "application/json",
+        "X-ClientTraceId": uuidv4().toString(),
+      },
+      body: texts,
+      json: true,
+    };
+
+    request(options, function (err, res, body) {
+      body.forEach((result, j) => {
+        oldToNewVal[allTexts[i + j].text] = result.translations[0].text;
+      });
+    }).then((res2) => {
+      let copy = _.cloneDeep(JSONOg);
+      updateOriginalJSON(copy);
+      let data = JSON.stringify(copy);
+      fs.writeFileSync(`${fileName}.${lang}.json`, data);
+    });
+  }
+
+  createTexts(JSON);
+
+  for (let i = 0; i < allTexts.length; i += 100) {
+    translateText(allTexts.slice(i, i + 100), i, lang);
+  }
+
+  function updateOriginalJSON(json) {
+    Object.keys(json).forEach((key) => {
+      if (
+        key !== "labelColor" &&
+        key !== "lang" &&
+        key !== "href" &&
+        key !== "target"
+      ) {
+        let ogVal = json[key];
+        if (typeof ogVal === "string") {
+          if (oldToNewVal[ogVal]) {
+            json[key] = oldToNewVal[ogVal];
+          }
+        } else if (typeof ogVal === "object") {
+          updateOriginalJSON(ogVal);
+        } else if (Array.isArray(ogVal)) {
+          ogVal.forEach((val) => {
+            updateOriginalJSON(val);
+          });
+        }
+      }
+    });
+  }
 }
+
+["fr", "de", "ja", "ko", "es", "pt"].forEach((lang) => translate(lang));
